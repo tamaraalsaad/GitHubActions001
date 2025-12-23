ccba173f2db40be0b00c8b0d5e333c156541ca0f
tamaraalsaad <tamaraalsaad@yahoo.com>
2025-12-23 12:02:12 +0100

    Initial commit

---
+++ src/test/java/Calculator/MyClassTest.java
@@ -0,0 +1,24 @@
+package Calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyClassTest {
+    @Test
+    public void testAdd() {
+        MyClass myClass = new MyClass();
+        assertEquals(3, myClass.add(1, 2));
+        assertEquals(0, myClass.add(-1, 1));
+        assertEquals(0, myClass.add(0, 0));
+    }
+
+    @Test
+    public void subAdd() {
+        MyClass myClass = new MyClass();
+        assertEquals(1, myClass.sub(2, 1));
+        assertEquals(-2, myClass.sub(-1, 1));
+        assertEquals(0, myClass.sub(0, 0));
+
+    }
+}
